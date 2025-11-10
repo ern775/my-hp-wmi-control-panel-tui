8d52a4edc262bcb053936e149efea637a2e9e6fa
berk-efe <berkefekeskin@gmail.com>
2025-11-10 20:43:47 +0300

    added some I/O

--- src/widgets/heat_widget.rs
+++ src/widgets/heat_widget.rs
@@ -7,9 +7,14 @@ use std::{
 use glob::glob;
 use ratatui::{
     buffer::Buffer,
-    style::Stylize,
+    layout::Direction,
+    style::{Color, Style, Styled, Stylize},
+    symbols::Marker,
     text::Line,
-    widgets::{Block, BorderType, Borders, Paragraph, Widget},
+    widgets::{
+        Axis, Bar, BarChart, BarGroup, Block, BorderType, Borders, Chart, Dataset, GraphType,
+        Padding, Paragraph, Widget,
+    },
 };
 
 pub struct HeatWidget {
@@ -55,25 +60,60 @@ impl HeatWidget {
                 panic!("cpu returned empty temp")
             }
 
-            res_str.trim().parse().expect(&format!(
+            let degrees: u64 = res_str.trim().parse().expect(&format!(
                 "failed to parse the output for get_cpu_temp : {} :",
                 res_str
-            ))
+            ));
+            degrees / 1000
         } else {
             let err_msg = String::from_utf8_lossy(&temp_output.stderr);
             panic!("{}", err_msg);
         }
     }
+
+    pub fn temperature_style(&self, value: u8) -> Style {
+        let green = (255.0 * (1.0 - f64::from(value.saturating_sub(50)) / 40.0)) as u8;
+        let color = Color::Rgb(255, green, 0);
+        Style::new().fg(color)
+    }
 }
 
 impl Widget for &HeatWidget {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut Buffer) {
-        Block::default()
+        let block = Block::default()
             .borders(Borders::ALL)
             .border_type(BorderType::Rounded)
             .title(self.title)
-            .render(area, buf);
+            .padding(Padding::uniform(1));
 
-        Paragraph::new(format!("temp: {}", self.get_cpu_temp())).render(area, buf);
+        // Create the datasets to fill the chart with
+        let temp1 = self.get_cpu_temp();
+        let temp2 = 55;
+
+        let style1 = self.temperature_style(temp1 as u8);
+        let style2 = self.temperature_style(temp2 as u8);
+
+        let bars: Vec<Bar> = vec![
+            Bar::default()
+                .value(temp1)
+                .label(Line::from("temp1"))
+                .text_value(format!("{}C", temp1))
+                .style(style1)
+                .value_style(style1.reversed()),
+            Bar::default()
+                .value(temp2)
+                .label(Line::from("temp2"))
+                .text_value(format!("{}C", temp2))
+                .style(style2)
+                .value_style(style2.reversed()),
+        ];
+
+        BarChart::default()
+            .block(block)
+            .data(BarGroup::default().bars(&bars))
+            .bar_width(10)
+            .bar_gap(2)
+            .direction(Direction::Vertical)
+            .render(area, buf);
     }
 }
